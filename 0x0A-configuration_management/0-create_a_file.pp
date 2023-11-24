d1350a7c5802fe21b10c845b1981c9bbe06057cb
Nkemjika1divine <cndivine@gmail.com>
2023-11-24 03:59:17 -0800

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This creates a file /tmp/school
 
 file { '/tmp/school':
-	ensure	=> file,
-	mode	=> 0744,
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content	=> 'I love Puppet'
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
